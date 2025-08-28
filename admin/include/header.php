a0536f518da2c97fbf8e17bb3e9a9249acab2886
Nandhini242003 <nandhini.zerosoft@gmail.com>
2025-08-28 17:32:22 +0530

    Update header.php

--- admin/include/header.php
+++ admin/include/header.php
@@ -1,3 +1,11 @@
+<?php
+// Start a session if one is not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$sitetitle=(isset($sitetitle)?(!empty($sitetitle)?$sitetitle:"Admin Panel 1"):"Admin Panel 3");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,75 +15,76 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/admin_styles.css"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> </head>
-<body>
-    <div class="dashboard-container">
-        ```
-
-**2. Modify `index.php`**
-
-This file will now include the main content within the new layout. You will need to move the sidebar and the main content into this file, as your `header.php` no longer contains the `wrapper` or `main-header` elements.
-
-```php
-<?php
-// dashboard.php - Admin dashboard
-error_reporting(1);
-ob_start();
-session_start();
-
-// Use your session variables
-if(empty($_SESSION['inktank_id']) && empty($_SESSION['inktank_username'])){
-    $_SESSION["cms_status"]="error";
-    $_SESSION["cms_msg"]="Please login now!";
-    header('Location:login.php');
-    exit();
-}
-
-include("../../lib/config.php");
-// Assuming you have site title defined in config
-$sitetitle = "Welcome | " . $db->select_query("SELECT setting_value FROM site_settings WHERE setting_name = 'site_title' LIMIT 1")[0]['setting_value'];
-include("include/header.php");
-?>
-
-<div class="main-content">
-    <div class="top-bar">
-        <h1 class="page-title">Welcome <small>Control panel</small></h1>
-        <div class="user-info">
-            <img src="images/avatar.png" class="user-image" alt="User Image">
-            <span>
-                <?php
-                if(!empty($_SESSION["inktank_username"])){
-                    echo htmlspecialchars($_SESSION["inktank_username"]);
-                } else {
-                    echo "User";
-                }
-                ?>
-            </span>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/datepicker.css"/>
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="css/select2.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/skins/skin-custom-blue.css">
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script src="js/jquery.repeater.js"></script>
+    <script src="js/jquery.form-repeater.js" type="text/javascript"></script>
+</head>
+<body class="hold-transition skin-blue sidebar-mini fixed">
+<div class="wrapper">
+<header class="main-header">
+    <a href="../index.php" class="logo" target="_blank" style="height: 100px;border: none;">
+        <span class="logo-lg">
+            <img src="images/logo.jpg" width="180" height="80"/>
+        </span>
+    </a>
+    <nav class="navbar navbar-static-top" style="height: 25px;">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="images/avatar.png" class="user-image" alt="User Image">
+                        <span class="hidden-xs">
+                            <?php
+                            if(!empty($_SESSION["inktank_username"])){
+                                echo $_SESSION["inktank_username"];
+                            }
+                            else{
+                                echo "User";
+                            }
+                            ?>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="user-header">
+                            <img src="images/avatar.png" class="img-circle" alt="User Image">
+                            <p>
+                                <?php
+                                if(!empty($_SESSION["inktank_username"])){
+                                    echo $_SESSION["inktank_username"]." - Administrator";
+                                }
+                                else{
+                                    echo "User - Unknown";
+                                }
+                                ?>
+                                <small></small>
+                            </p>
+                        </li>
+                        <li class="user-body">
+                            <div class="row">
+                            </div>
+                        </li>
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="changepassword.php" class="btn btn-default btn-flat">Change Password</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="logout.php" class="btn btn-default btn-flat">Log out</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
-    </div>
-    
-    <div class="content-body">
-        <h4 class="welcome-heading">Welcome to <b>INKTANK Admin Panel</b>!</h4>
-        <p class="overview-text">Please choose an option from the left menu.</p>
-        
-        <div class="info-cards-container">
-            <div class="info-card">
-                <div class="card-title">Total Users</div>
-                <div class="card-number">1,234</div>
-            </div>
-            <div class="info-card">
-                <div class="card-title">Total Posts</div>
-                <div class="card-number">567</div>
-            </div>
-            <div class="info-card">
-                <div class="card-title">New Comments</div>
-                <div class="card-number">89</div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php 
-// Assuming you have a footer file
-include("include/footer.php");
-?>
+    </nav>
+</header>
